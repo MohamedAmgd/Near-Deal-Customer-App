37ea4df8b47e32aa8196f918b3dcd7a02e0dab5e
Mohamed Amgd <megapof2@gmail.com>
2021-04-07 01:25:29 +0200

    added product fragment ui

---
+++ app/src/main/java/com/mohamed_amgd/ayzeh/ProductFragment.java
@@ -0,0 +1,36 @@
+package com.mohamed_amgd.ayzeh;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProductFragment extends Fragment {
+
+    private ProductViewModel mViewModel;
+
+    public static ProductFragment newInstance() {
+        return new ProductFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.product_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/mohamed_amgd/ayzeh/ProductViewModel.java
@@ -0,0 +1,7 @@
+package com.mohamed_amgd.ayzeh;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProductViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
