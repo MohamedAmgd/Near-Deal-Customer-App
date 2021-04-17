7b82f5316d8197a603e6d0fb02e284ac0436826b
Mohamed Amgd <megapof2@gmail.com>
2021-04-17 19:25:20 +0200

    added edit user ui

---
+++ app/src/main/java/com/mohamed_amgd/ayzeh/EditUserFragment.java
@@ -0,0 +1,36 @@
+package com.mohamed_amgd.ayzeh;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EditUserFragment extends Fragment {
+
+    private EditUserViewModel mViewModel;
+
+    public static EditUserFragment newInstance() {
+        return new EditUserFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.edit_user_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(EditUserViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/mohamed_amgd/ayzeh/EditUserViewModel.java
@@ -0,0 +1,7 @@
+package com.mohamed_amgd.ayzeh;
+
+import androidx.lifecycle.ViewModel;
+
+public class EditUserViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
