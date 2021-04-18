bdfc1ea72b216727badc6b2857fb4ed5f3b7a16d
Mohamed Amgd <megapof2@gmail.com>
2021-04-18 18:09:39 +0200

    added shop info ui

---
+++ app/src/main/java/com/mohamed_amgd/ayzeh/ShopInfoFragment.java
@@ -0,0 +1,36 @@
+package com.mohamed_amgd.ayzeh;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShopInfoFragment extends Fragment {
+
+    private ShopInfoViewModel mViewModel;
+
+    public static ShopInfoFragment newInstance() {
+        return new ShopInfoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.shop_info_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ShopInfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
