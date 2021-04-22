342f8cb0938566bdc526833761159a923ab2dc2f
Mohamed Amgd <megapof2@gmail.com>
2021-04-22 03:44:54 +0200

    added nearby locations ui

---
+++ app/src/main/java/com/mohamed_amgd/ayzeh/NearbyLocationsFragment.java
@@ -0,0 +1,36 @@
+package com.mohamed_amgd.ayzeh;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NearbyLocationsFragment extends Fragment {
+
+    private NearbyLocationsViewModel mViewModel;
+
+    public static NearbyLocationsFragment newInstance() {
+        return new NearbyLocationsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.nearby_locations_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(NearbyLocationsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/mohamed_amgd/ayzeh/NearbyLocationsViewModel.java
@@ -0,0 +1,7 @@
+package com.mohamed_amgd.ayzeh;
+
+import androidx.lifecycle.ViewModel;
+
+public class NearbyLocationsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
