d28c54a1150b2362c6d57b668523bfff8c67ce4f
Mohamed Amgd <megapof2@gmail.com>
2021-05-27 08:35:14 +0200

    added util implementaion

--- app/src/main/java/com/mohamed_amgd/ayzeh/repo/Util.java
+++ app/src/main/java/com/mohamed_amgd/ayzeh/repo/Util.java
@@ -1,35 +1,62 @@
 package com.mohamed_amgd.ayzeh.repo;
 
+import android.util.Patterns;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
 public class Util {
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
     private static Util mInstance;
 
     private Util() {
     }
 
     public static Util getInstance() {
-        if (mInstance == null){
+        if (mInstance == null) {
             mInstance = new Util();
         }
         return mInstance;
     }
 
-    public boolean isEmailValid(String email){
-        return true;
+    public boolean isEmailValid(String email) {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
     }
 
     public boolean isUsernameValid(String username) {
-        return true;
+        Pattern pattern = Pattern.compile("^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$");
+        return pattern.matcher(username).matches();
     }
 
     public boolean isPasswordValid(String password) {
-        return true;
+        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
+        return pattern.matcher(password).matches();
     }
 
     public boolean isConfirmPasswordValid(String password, String confirmPassword) {
-        return true;
+        return password.equals(confirmPassword);
     }
 
-    public boolean isBirthdateValid(String birthdate) {
-        return true;
+    public boolean isBirthdateValid(String birthdateInput) {
+        Pattern pattern = Pattern.compile("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+        if(pattern.matcher(birthdateInput).matches()){
+            SimpleDateFormat sdf = new SimpleDateFormat(Util.DATE_FORMAT, Locale.US);
+            Date birthdate = null;
+            try {
+                birthdate = sdf.parse(birthdateInput);
+            } catch (ParseException e) {
+                return false;
+            }
+            Date currentDate = Calendar.getInstance().getTime();
+            long diffInDays = (currentDate.getTime() - birthdate.getTime())/(1000L*60L*60L*24L*365L);
+            return diffInDays >= 12;
+        } else {
+            return false;
+        }
     }
 }
